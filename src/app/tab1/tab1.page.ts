96f96c3f23f8bdedbd77c57749f1399fd4b1a21b
ramkishor-code <80231296+ramkishor-code@users.noreply.github.com>
2021-07-21 08:13:08 -0700

    First commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-
+import { NavController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -7,6 +9,79 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(public navCtrl: NavController, public toastCtrl: ToastController, public alertCtrl: AlertController)
+   {}
+  items = [
+    {
+      name: "Milk",
+      quantity: 2    
+    },
+    {
+      name: "Bread",
+      quantity: 1    
+    },
+    {
+      name: "Banana",
+      quantity: 3    
+    },
+    {
+      name: "Sugar",
+      quantity: 1    
+    },
+  ];
+  addItem() {
+    console.log("Adding Item");
+    this.showAddItemPrompt();
+  }
+ async showAddItemPrompt() {
+    const prompt = await this.alertCtrl.create({
+      
+      message: "Please enter item...",
+      inputs: [
+        {
+          name: 'name',
+          placeholder: 'Name'
+        },
+        {
+          name: 'quantity',
+          placeholder: 'Quantity'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: item => {
+            console.log('Saved clicked', item);
+            this.items.push(item);
+          }
+        }
+      ]
+    });
+    await prompt.present();
+  }
 
+ async removeItem(item) {
+    console.log("Removing Item - ", item);
+    
+    const toast = await this.toastCtrl.create({
+      message: 'Removing Item - ' + item.name + " ...",
+      duration: 3000
+    });
+    
+    await toast.present();
+    let afterfilter = [];
+for (let i = 0; i < this.items.length; i++) {
+    if (this.items[i].name != item.name) {
+        afterfilter.push(this.items[i]);
+    }
+}
+this.items=afterfilter;
+    //this.items = this.items.filter(item => item.name === item.name);
+  }
 }
